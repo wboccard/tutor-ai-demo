060a6cad6fe17af21411f5fd0d2ec8cf5389fcb6
wboccard <72316984+wboccard@users.noreply.github.com>
2023-08-02 23:38:18 -0400

    improves ffmpeg conversion

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ from werkzeug.utils import secure_filename
 from improved_chatbot import Chatbot
 import os
 import subprocess
-import time
 
-UPLOAD_FOLDER = 'static\\uploads'
+UPLOAD_FOLDER = os.path.join("static", "uploads")
 # ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
@@ -25,15 +24,15 @@ def print_audio():
         flash('No file part')
         return redirect(request.url)
     file = request.files['file']
-    filename = file.filename
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    initial_file = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    final_file = os.path.join(app.config["UPLOAD_FOLDER"], "fixed.flac")
-    subprocess.call(["ffmpeg.exe", "-hide_banner", "-loglevel", "error", "-y", "-i", initial_file,
-                        "-codec", "flac", final_file])
-    with open(os.path.join(app.config['UPLOAD_FOLDER'], "fixed.flac"), "rb") as question:
-        audio = question.read()
-        speech_to_text = google_chatbot.get_speech_to_text(audio)
+    file_audio = file.read()
+    pipe = subprocess.Popen(
+        'ffmpeg -i pipe: -f flac pipe:'
+        .split(),
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE)
+    audio, _ = pipe.communicate(file_audio)
+    speech_to_text = google_chatbot.get_speech_to_text(audio)
     if not speech_to_text:
         speech_to_text = "Woops, I didn't say anything!"
         return speech_to_text
@@ -43,7 +42,6 @@ def print_audio():
 @app.route('/askChat', methods=["POST"])
 def askChatbot():
     request_json = request.get_json()
-    # print(request_json['question'])
     try:
         answer = google_chatbot.ask_chat(request_json['question'])
     except:
@@ -55,10 +53,6 @@ def get_speech():
     request_json = request.get_json()
     audio = google_chatbot.text_to_speech(request_json['text_to_speech'])
     return audio
-
-# @app.route('/<audio_file_name>')
-# def returnAudioFile(audio_file_name):
-#     return send_from_directory(app.config['UPLOAD_FOLDER'], audio_file_name)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
